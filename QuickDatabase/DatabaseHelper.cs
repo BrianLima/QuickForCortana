9f0a68c917f37713ee403e2d5d1e1cd911d1d08b
Brian Lima <brian.ghwt@outlook.com>
2015-09-07 11:08:38 -0300

    Building database and SQLite references.

---
+++ QuickDatabase/DatabaseHelper.cs
@@ -0,0 +1,50 @@
+﻿using SQLite;
+using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace QuickDatabase
+{
+    public class DatabaseHelper
+    {
+        private String DB_NAME = "QuickDatabase.db";
+
+        public SQLiteAsyncConnection Conn { get; set; }
+
+        public DatabaseHelper()
+        {
+            Conn = new SQLiteAsyncConnection(DB_NAME);
+            this.CreateDb();
+        }
+
+        public async void CreateDb()
+        {
+            bool dbExist = await CheckDbAsync();
+            if (!dbExist)
+            {
+                await CreateDatabaseAsync();
+            }
+        }
+
+        public async Task<bool> CheckDbAsync()
+        {
+            bool dbExist = true;
+
+            try
+            {
+                StorageFile sf = await ApplicationData.Current.LocalFolder.GetFileAsync(DB_NAME);
+            }
+            catch (Exception)
+            {
+                dbExist = false;
+            }
+
+            return dbExist;
+        }
+
+        private async Task CreateDatabaseAsync()
+        {
+            await Conn.CreateTableAsync<DbNotes>();
+        }
+    }
+}
---
+++ QuickDatabase/DbNotes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QuickDatabase
+{
+    internal class DbNotes
+    {
+        public long Id { get; internal set; }
+        public DateTime date;
+        public string note;
+        public string vat;
+
+        public DbNotes()
+        {
+        }
+
+    }
+}
