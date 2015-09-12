003a964ac2831d00902abcf64553bf8f6dee2db4
Brian Lima <brian.ghwt@outlook.com>
2015-09-12 19:05:16 -0300

    Reading and storing data on a JSON file

---
+++ QuickStorage/Note.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickStorage
+{
+    public class Note
+    {
+
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private string _note;
+
+        public string note
+        {
+            get { return note; }
+            set { note = value; }
+        }
+
+        private DateTime _date;
+
+        public DateTime date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+
+
+    }
+}
